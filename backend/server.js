32eeeedd1e8d36594f142e2c43bd26162c082fb1
James Talamo <jamestalamo4@gmail.com>
2024-11-05 15:40:07 +0800

    production

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 let connectDB = require('./config/db.js')
 
-
+let path = require('path')
 
 
 //Server
@@ -13,6 +13,12 @@ app.use(express.json())
 
 app.use('/api/product', require('./route/ProductRoute.js'))
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
 
 
 let PORT = process.env.PORT || 5000
